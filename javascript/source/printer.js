cc81036da7fdb2bb5d09beff1e06acd50de74f60
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-10 00:25:03 +0900

    🌱: 오타 수정
    해당 코드는 Github에서 직접 수정

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,6 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().split('\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');  //배준 제출
+//let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');  //백준 제출
 
 console.log('Hello World!');
