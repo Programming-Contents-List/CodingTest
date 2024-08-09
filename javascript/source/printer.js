b33737d58df20ffc9bda3a7f6e7f4847c154118b
4Bee <4bee.code@gmail.com>
2024-08-09 23:09:17 +0900

    🚩: 오답 제출

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -2,4 +2,16 @@ let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().split('\n');
 
-console.log('Hello World!');
+let [n, k] = input[0].trim().split(' ').map(Number);
+console.log(n, k);
+let values = input[1].trim().split(' ').map(Number);
+console.log(values);
+
+for (let i = 0; i < values.length; i++) { //기준이 되는 배열
+  for (let j = 0; j < values.length; j++) {
+    if (values[j] > values[j] - n) {
+      values[j], values[j + 1] = values[j + 1], values[j];
+    }
+  }
+  console.log(values);
+}
