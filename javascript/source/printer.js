6f999c2d56cee62e049eef8283c20145c7e2e200
4Bee <4bee.code@gmail.com>
2024-08-09 23:50:32 +0900

    🚩: 코드의 log를 찍기 위한 커밋

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -11,9 +11,15 @@ console.log(values);
 for (let i = 0; i < k; i++) { //기준이 되는 배열
   let swapped = false;
   for (let j = 0; j < n - 1; j++) { //for (let j = 0; j < values.length; j++) {
+    console.log('i', values[i]);
+    console.log('j', values[j]);
+    console.log('j+1', values[j + 1]);
+    console.log('values', values);
     if (values[j] > values[j + 1]) {  //if (values[j] < values[n - 1]) {
       [values[j], values[j + 1]] = [values[j + 1], values[j]];
+      let result = [values[j], values[j + 1]] = [values[j + 1], values[j]];
       swapped = true;
+      console.log('result', result, ' ' + swapped);
     }
   }
   if (!swapped) break;
