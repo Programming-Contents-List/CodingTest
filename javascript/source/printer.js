f822ef523e8c486ce883068023f8065d902920fa
4Bee <4bee.code@gmail.com>
2024-08-10 21:00:29 +0900

    🚩: 문제 유의 사항 주석으로 작성

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -4,3 +4,4 @@ let input = require('fs').readFileSync(inputPath).toString().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
 
 console.log('Hello, World!');
+//2738문제의 A와 B를 나누는 기누은 N과 M을 곱한(N*M)의 각 크기만큼이다. 이점을 유의 해서 문제를 접근해야한다.
