00625a6e3030f5377bdf50434522e0b465c947a0
4Bee <4bee.code@gmail.com>
2024-08-10 16:50:23 +0900

    🐛: 문제 해결 및 원인 분석

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,21 +1,20 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().split('\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');  //백준 제출
+//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
 
-let N = parseInt(input[0]);
+let N = input[0];
 console.log(N);
-let arr = input[1].trim().split(' ').map(Number);
+let arr = input[1].split(' ');
 console.log(arr);
-let V = parseInt(input[2]);;
+let V = input[2];
 console.log(V);
-let result = 0;
-//V가 arr안에 있는지 확인을 한다. 결과물은 V가 arr몇번째에 있는지 찾는 것이다.
+let count = 0;
+//V가 arr안에 있는지 확인을 한다. 결과물은 V가 arr 배열에 몇개가 존재 하는지 찾는 것이다.
 
-for (let i = 0; i < arr.length; i++) {
+for (let i = 0; i < N; i++) {
   if (V === arr[i]) {
-    result = i;
-    break;
+    count += 1;
   }
 }
-console.log(result);
+console.log(count);
