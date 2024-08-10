bae8a717bd17a09f4613348768c2bf2f900aa7d0
4Bee <4bee.code@gmail.com>
2024-08-10 16:31:54 +0900

    🚩: 문제 오독으로 인한 오답

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -3,4 +3,19 @@ let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스�
 let input = require('fs').readFileSync(inputPath).toString().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');  //백준 제출
 
-console.log('Hello World!');
+let N = parseInt(input[0]);
+console.log(N);
+let arr = input[1].trim().split(' ').map(Number);
+console.log(arr);
+let V = parseInt(input[2]);;
+console.log(V);
+let result = 0;
+//V가 arr안에 있는지 확인을 한다. 결과물은 V가 arr몇번째에 있는지 찾는 것이다.
+
+for (let i = 0; i < arr.length; i++) {
+  if (V === arr[i]) {
+    result = i;
+    break;
+  }
+}
+console.log(result);
