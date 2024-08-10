9ec121daadf1b0b2396f5424a0007e5b24880cc7
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-10 17:37:43 +0900

    10807 (#4)
    * 🌱: 백준 제출 필수 코드 삽입
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: 오타 수정
    
    해당 코드는 Github에서 직접 수정
    
    * 🚩: 문제 오독으로 인한 오답
    
    * 🐛: 문제 해결 및 원인 분석

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,21 +1,20 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().split('\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');  //배준 제출
+//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
 
-let [n, k] = input[0].trim().split(' ').map(Number);
-console.log(n, k);
-let values = input[1].trim().split(' ').map(Number);
-console.log(values);
+let N = input[0];
+console.log(N);
+let arr = input[1].split(' ');
+console.log(arr);
+let V = input[2];
+console.log(V);
+let count = 0;
+//V가 arr안에 있는지 확인을 한다. 결과물은 V가 arr 배열에 몇개가 존재 하는지 찾는 것이다.
 
-for (let i = 0; i < k; i++) { //기준이 되는 배열
-  let swapped = false;
-  for (let j = 0; j < n - 1; j++) { //for (let j = 0; j < values.length; j++) {
-    if (values[j] > values[j + 1]) {  //if (values[j] < values[n - 1]) {
-      [values[j], values[j + 1]] = [values[j + 1], values[j]];
-      let swapped = true;
-    }
+for (let i = 0; i < N; i++) {
+  if (V === arr[i]) {
+    count += 1;
   }
-  if (!swapped) break;
 }
-console.log(values.join(' '));  // 상위 k개의 값을 출력
+console.log(count);
