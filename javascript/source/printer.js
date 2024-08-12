e800eeaa94b38a75ce019963e9a2d8f7518b5f79
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-13 00:05:21 +0900

    🔀: 2750 (#10)
    * 🚩: 문제를 풀기위한 세팅
    
    * 🚩: PR 템플릿 생성 문제로 파일 삭제
    
    * 🚩: bubble 정렬 적용 및 정렬 확인
    
    * 🚩: 배열 중복제거 로직 구현 오답
    
    * 🌱 : PR 템플릿 재생성

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -2,5 +2,23 @@ let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
+console.log(input);
 
-console.log('Hello, World!');
+let arr = [];
+for (let i = 0; i < input.length; i++) {
+  if (!arr.includes(input[i])) {
+    arr.push(input[i]);
+  }
+}
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length; j++) {
+    //for (let j = 0; j < arr.length - 1 - i; j++) { // 내부 반복문에서 비교 범위 감소
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+  }
+}
+
+console.log(arr);
+arr.forEach(element => console.log(element));
