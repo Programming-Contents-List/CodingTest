7502a33d7ef69e3cd3d5bcaf321d778c1fc70267
4Bee <4bee.code@gmail.com>
2024-08-12 23:48:12 +0900

    🚩: 배열 중복제거 로직 구현 오답

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -4,13 +4,21 @@ let input = require('fs').readFileSync(inputPath).toString().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
 console.log(input);
 
+let arr = [];
 for (let i = 0; i < input.length; i++) {
-  for (let j = 0; j < input.length; j++) {
-    if (input[j] > input[j + 1]) {
-      [input[j], input[j + 1]] = [input[j + 1], input[j]];
+  if (!arr.includes(input[i])) {
+    arr.push(input[i]);
+  }
+}
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length; j++) {
+    //for (let j = 0; j < arr.length - 1 - i; j++) { // 내부 반복문에서 비교 범위 감소
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
     }
   }
 }
 
-console.log(input);
-input.forEach(element => console.log(element));
+console.log(arr);
+arr.forEach(element => console.log(element));
