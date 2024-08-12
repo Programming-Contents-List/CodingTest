3bb83a9a6427b29eb7e6d8d245b90d63c5f74d2c
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-12 22:54:57 +0900

    🔀: 2738 (#8)
    * 🌱: 백준 제출 필수 코드 삽입
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: 오타 수정
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: issue templates 생성
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: issue templates 수정
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: pr templates  생성
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱:  README을 수정
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: printer.js 파일 수정
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: stdin 수정
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: printer.js 파일 수정
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: printer.js 파일 수정
    
    해당 코드는 Github에서 직접 수정
    
    * 🚩: 문제 유의 사항 주석으로 작성
    
    * 🚩: 변수 설정
    
    * 🚩: 2738 입력값 설정
    
    * 🐛: 런타임 에러
    
    * 🐛: New Array 방법 시도
    
    * ⏪: Revert "🐛: 런타임 에러"
    
    * 🐛: 런타임 문제 해결 원인은 A와 B를 map으로 직접 지정을 해서 문제 new Array로 풀어 내는 방법도 구현 필요
    
    * 🚩: 설명 및 정리가 필요한 부분 주석으로 처리

--- README.md
+++ README.md
@@ -2,5 +2,72 @@
 
 ## Rules
 
-- 2557번 문제를 기준으로 main branch에 파일과 폴더가 작성 되어 있다.
+### Branch
+
+- main
+  - 2557번 문제를 기준으로 main branch에 파일과 폴더가 작성 되어 있다.
+  - main은 기본 세팅이 되어 있기 때문에 pr이나 외부 push가 이루어져선 안된다.
+- recently
+  - 최근 풀어본 문제가 담겨 있는 브렌치다.
+
+---
+
+### PR
+
 - 새로운 문제를 풀때마다 해당 문제 번호로 브렌치를 생성하고 pr을 작성한다.
+    <details>
+      <summary><b>PR-template</b></summary>
+
+      ```
+        ## #️⃣ Number
+
+      - problem Number
+
+      ## 🏆정답여부
+
+      - [ ] 정답
+
+      ## 🌐문제 링크
+
+      🔗 [문제번호](https://# "baekjoon 문제번호")
+
+      ## 🔐풀이 과정
+
+      - 풀이과정을 적어 주세요.
+
+      ## 📌Point
+
+      - 구체적인 풀이 과정을 적어 주세요.
+
+      ## 🔔Notice
+
+      - 🐛 #오답 문제 issue 번호
+      - 🔄 #다시 풀어볼 issue 번호
+
+      ```
+
+    </details>
+
+---
+
+### issue
+
+- 문제를 풀고 오답 또는 재시도가 필요한 문제들은 issue를 생성해서 관리한다.
+  <details>
+    <summary><b>issue-template</b></summary>
+
+  ```
+  ## #️⃣PR Number
+
+  - #PR
+
+  ## 🌐문제 링크
+
+  🔗 [문제번호](https://# "baekjoon 문제번호")
+
+  ## 📌Point
+
+  - 오류 원인을 찾아내고 문제 해결
+  ```
+
+  </details>
--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -3,18 +3,28 @@ let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스�
 let input = require('fs').readFileSync(inputPath).toString().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
 
-let N = input[0];
-console.log(N);
-let arr = input[1].split(' ');
-console.log(arr);
-let V = input[2];
-console.log(V);
-let count = 0;
-//V가 arr안에 있는지 확인을 한다. 결과물은 V가 arr 배열에 몇개가 존재 하는지 찾는 것이다.
+let [N, M] = input[0].trim().split(' ').map(Number);
+//왜 map을 slice를 사용해서 변수값을 정의하는 방법과 new Array를 사용해서 크기를 지정하는 방식의 차이가 무엇인지 설명
+// let A = input.slice(1, 4).map(row => row.trim().split(' ').map(Number));
+// let B = input.slice(4, 7).map(row => row.trim().split(' ').map(Number));
+//배열 초기 값
+let arr = [];
 
+// console.log(A, B);
+//2738문제의 A와 B를 나누는 기누은 N과 M을 곱한(N*M)의 각 크기만큼이다. 이점을 유의 해서 문제를 접근해야한다.
 for (let i = 0; i < N; i++) {
-  if (V === arr[i]) {
-    count += 1;
+  let row = [];
+  for (let j = 0; j < M; j++) {
+    // let result = A[i][j] + B[i][j];
+    //왜 i+1, i+1+N 을 지정해야하는 설명
+    let A = parseInt(input[i + 1].trim().split(' ')[j]);
+    let B = parseInt(input[i + 1 + N].trim().split(' ')[j]);
+    row.push(A + B);
+    //trim과 split을 사용해야하는 이유
+    // console.log('a+b: ', B);
+    // row.push(result);
   }
+  arr.push(row);
 }
-console.log(count);
+
+arr.forEach(element => console.log(element.join(' ')));
