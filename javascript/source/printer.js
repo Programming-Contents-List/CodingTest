27013f5edce5373698df3a590008cea473e41a69
4Bee <4bee.code@gmail.com>
2024-08-12 15:35:49 +0900

    🐛: New Array 방법 시도

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -5,17 +5,18 @@ let input = require('fs').readFileSync(inputPath).toString().split('\n');
 let [N, M] = input[0].trim().split(' ').map(Number);
 let A = input.slice(1, 4).map(row => row.trim().split(' ').map(Number));
 let B = input.slice(4, 7).map(row => row.trim().split(' ').map(Number));
-let arr = [];
+//배열 초기 값
+let arr = new Array(N);
+for (let i = 0; i < N; i++) {
+  arr[i] = new Array(M);
+}
 
 // console.log(A, B);
 //2738문제의 A와 B를 나누는 기누은 N과 M을 곱한(N*M)의 각 크기만큼이다. 이점을 유의 해서 문제를 접근해야한다.
 for (let i = 0; i < N; i++) {
-  let row = [];
   for (let j = 0; j < M; j++) {
-    let result = A[i][j] + B[i][j];
-    row.push(result);
+    arr[i][j] = A[i][j] + B[i][j];
   }
-  arr.push(row);
 }
 
 arr.forEach(element => console.log(element.join(' ')));
