ed51239453b82bcf4e5829ad419395b67ca7618b
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-15 11:53:41 +0900

    🔀: 2750 retry(1) (#12)
    * 🚩: 문제를 풀기위한 세팅
    
    * 🚩: PR 템플릿 생성 문제로 파일 삭제
    
    * 🚩: bubble 정렬 적용 및 정렬 확인
    
    * 🚩: 배열 중복제거 로직 구현 오답
    
    * 🌱 : PR 템플릿 재생성
    
    * 🐛: sort와 filter 그리고 indexOf 사용해서 문제 해결 시도, 오답
    
    * 🐛: shift와 trim을 사용해서 문제 해결, pop으로 문제를 시도 했으나 오답 왜인지 아직 명확하지 않다.
    
    * 🚩: 주석제거

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,24 +1,8 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().split('\n');
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
 console.log(input);
-
-let arr = [];
-for (let i = 0; i < input.length; i++) {
-  if (!arr.includes(input[i])) {
-    arr.push(input[i]);
-  }
-}
-
-for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr.length; j++) {
-    //for (let j = 0; j < arr.length - 1 - i; j++) { // 내부 반복문에서 비교 범위 감소
-    if (arr[j] > arr[j + 1]) {
-      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-    }
-  }
-}
-
-console.log(arr);
+input.shift();
+const arr = input.sort((a, b) => a - b);  //암묵적으로 숫자로 형변환
 arr.forEach(element => console.log(element));
