3511d74417f0da37643238f72b01cb0b67486615
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-21 00:10:58 +0900

    🔀: 1181 (#14)
    * 🌱: 알고리즘 개념을 위한 issue templates 생성
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: main 브렌치 환경 초기 세팅을 위한 커밋
    
    * 🚩: shift와 sort를 사용해서 정렬 시도
    
    * 🚩: sort의 오름차순 정렬 설정을 통해서 배열 길이 순으로 정렬 시도
    
    * 🚩: sort 비교함수를 if문으로 알파벳, 문자열 길이 기준으로 정렬이 될 수 있게 문제 해결

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,8 +1,16 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
-console.log(input);
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
 input.shift();
-const arr = input.sort((a, b) => a - b);  //암묵적으로 숫자로 형변환
-arr.forEach(element => console.log(element));
+let arr = [...new Set(input)];
+//sort의 조건을 다는 방법을 더욱 명확히 잡아 볼 것.
+let result = arr.sort((a, b) => {
+  if (a.length === b.length) {
+    return a.localeCompare(b);
+  } else {
+    return a.length - b.length;
+  }
+});
+
+result.forEach(element => console.log(element));
