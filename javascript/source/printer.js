ab233f11b14d914d2dc046b7b2852791140752aa
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-21 17:35:18 +0900

    🔀: 10798 (#16)
    * 🌱: 알고리즘 개념을 위한 issue templates 생성
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: main 브렌치 환경 초기 세팅을 위한 커밋
    
    * 🚩: 이중 for문을 사용해서 문제 해결 시도
    
    * 🐛: 첫 번째 예제 입력1은 맞지만 예제 입력2는 오답
    
    * 🐛: 백준은 정답이지만 vsc에서 출력되는 오답과 풀리지 않은 의문

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,16 +1,18 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
-input.shift();
-let arr = [...new Set(input)];
-//sort의 조건을 다는 방법을 더욱 명확히 잡아 볼 것.
-let result = arr.sort((a, b) => {
-  if (a.length === b.length) {
-    return a.localeCompare(b);
-  } else {
-    return a.length - b.length;
-  }
-});
 
-result.forEach(element => console.log(element));
+//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
+// input.forEach(el => console.log(el))
+let max = Math.max(...input.map(el => el.length));
+let result = "";
+for (let i = 0; i < max; i++) {
+  for (let j = 0; j < input.length; j++) {
+    if (input[j][i]) {
+      result += input[j][i];
+    }
+  }
+  console.log(result);
+
+}
+console.log(result);
