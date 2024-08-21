997f2a78cda64cc3f8ed81031451dc5f1f32535d
4Bee <4bee.code@gmail.com>
2024-08-21 12:55:42 +0900

    🐛: 백준은 정답이지만 vsc에서 출력되는 오답과 풀리지 않은 의문

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -3,17 +3,15 @@ let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스�
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
 // input.forEach(el => console.log(el))
+let max = Math.max(...input.map(el => el.length));
 let result = "";
-console.log(input[0][2]);
-for (let i = 0; i < input.length; i++) {
+for (let i = 0; i < max; i++) {
   for (let j = 0; j < input.length; j++) {
-    result += input[j][i];
-
-    //왜 [j][i]일 경우와 [i][j]일 경우의 결과 값이 다른가?
-    //이유는 j가 0번쩨 일때 [i++][0]
-    //하지만 2번째 입력인 경우는 불일치
-    // if (i < input.length) {
-    // }
+    if (input[j][i]) {
+      result += input[j][i];
+    }
   }
+  console.log(result);
+
 }
 console.log(result);
