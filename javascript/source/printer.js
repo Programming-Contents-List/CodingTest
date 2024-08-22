4d8644edd51142d02702741dc83a42110a83d763
4Bee <4bee.code@gmail.com>
2024-08-23 02:41:18 +0900

    🚩: 이중 for문을 사용해서 문제 시도

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,17 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
+let length = input[0];
+input.shift();
+// console.log(input, length);
 
-console.log('Hello, World!');
+// console.log(input[0][1])
+for (let i = 0; i < length; i++) {
+  let max = Math.max(input[i].length);
+  // console.log(max)
+  for (let j = 0; j < max; j++) {
+    // input[i][j].sort((a, b) => a - b);
+    console.log(input[i][j]);
+  }
+}
