22a960eeaab5620735b3189334fb7b2eb29af123
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-24 01:02:37 +0900

    🔀: 11650 (#19)
    * 🌱: 알고리즘 개념을 위한 issue templates 생성
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: main 브렌치 환경 초기 세팅을 위한 커밋
    
    * 🚩: 이중 for문을 사용해서 문제 시도
    
    * 🚩: sort 조건문을 사용해서 문제 시도 오답
    
    * 🚩: x,y 정렬 순서 및 기준을 수정해서 문제 시도 정답

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,18 +1,29 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
+let length = input[0];
+input.shift();
 
-//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
-// input.forEach(el => console.log(el))
-let max = Math.max(...input.map(el => el.length));
-let result = "";
-for (let i = 0; i < max; i++) {
-  for (let j = 0; j < input.length; j++) {
-    if (input[j][i]) {
-      result += input[j][i];
-    }
+let arr = input.map(el => el.split(' ')); //.map(Number)
+arr.sort((a, b) => {
+  if (a[0] === b[0]) {
+    return a[1] - b[1];  // y좌표가 같을 때 x좌표 오름차순
+  } else {
+    return a[0] - b[0];  // y좌표 오름차순
   }
-  console.log(result);
+});
 
-}
-console.log(result);
+// console.log(arr);
+
+arr.forEach(el => console.log(el.join(' ')));
+
+// console.log(input[0][1])
+// for (let i = 0; i < length; i++) {
+//   let max = Math.max(input[i].length);
+//   // console.log(max)
+//   for (let j = 0; j < max; j++) {
+//     // input[i][j].sort((a, b) => a - b);
+//     console.log(input[i][j]);
+//   }
+// }
