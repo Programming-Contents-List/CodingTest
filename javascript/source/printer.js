030793d5c5313dc93d404e9346d37d630b8c049e
4Bee <4bee.code@gmail.com>
2024-08-24 00:31:51 +0900

    🚩:  입력받은 값과 주어진 조건의 문제 파악으로 정답

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,11 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
-
-console.log('Hello, World!');
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
+let arr = input.map(el => el.split(' '));
+let N = arr[0][0] - 1;
+let X = arr[0][1] - 1;
+let score = arr[1];
+console.log(N, X);
+score.sort((b, a) => a - b);
+console.log(score[X]);
