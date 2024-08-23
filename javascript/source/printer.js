5c88483d66cc3f8fdd5351139999915416b57992
4Bee <4bee.code@gmail.com>
2024-08-24 01:49:07 +0900

    🚩: filter 함수를 사용해서 문제 해결 정답

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,20 +1,12 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().split('\n');
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
 
-let N = input[0];
-console.log(N);
-let arr = input[1].split(' ');
-console.log(arr);
-let V = input[2];
-console.log(V);
-let count = 0;
-//V가 arr안에 있는지 확인을 한다. 결과물은 V가 arr 배열에 몇개가 존재 하는지 찾는 것이다.
-
-for (let i = 0; i < N; i++) {
-  if (V === arr[i]) {
-    count += 1;
-  }
-}
-console.log(count);
+let temp = input.map(el => el.split(' '));
+// console.log(temp);
+let arr = temp[1];
+let v = temp[2];
+// console.log(v);
+let result = arr.filter(el => el == v).length;
+console.log(result);
