0995f6cf325097c79c3c49e5d6c843ee01f3a56f
4Bee <4bee.code@gmail.com>
2024-08-24 11:16:48 +0900

    🚩: for문과 if문을 통해 잔돈이 0이 될때 까지의 조건 적용 원하는 결과 미출력

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,21 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
+let N = [500, 100, 50, 10, 5, 1];
+let paid = 1000;
+let price = input[0];
+let charge = paid - price;
+console.log(charge);
 
-console.log('Hello, World!');
+for (let i = 0; i < N.length; i++) {
+  if (charge > N[i]) {
+    charge -= N[i];
+    console.log('used -', N[i]);
+    console.log('current', charge);
+  }
+  if (charge === 0) {
+    break;
+  }
+}
+console.log('done');
