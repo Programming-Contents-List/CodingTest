7c5b2bed5734e9e0e2ecc3aafdca2691a31e3cad
4Bee <4bee.code@gmail.com>
2024-08-25 14:23:36 +0900

    🚩: 누적의 합을 구하는 방식을 구현했지만 스코프에 따른 초기값으로 의도하는 값이 나오지 않는다. 오답

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,19 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
 
-console.log('Hello, World!');
+let temp = input.map(el => el.split(' ').map(Number));
+console.log(temp[0], temp[1]);
+
+let arr = temp[1]
+arr.sort((a, b) => a - b);
+console.log(arr);
+let result = 0;
+
+arr.forEach(el => {
+  let temp = el;
+  temp += el;
+  result += temp;
+});
+console.log(result);
