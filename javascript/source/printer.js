3e9b9a3b2c3e92ab9dbc1c83daf2d8e10e6f5337
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-26 15:05:57 +0900

    🌱: printer.js 파일 수정
    Github에서 수정한 커밋입니다.

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,6 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
 
 console.log('Hello, World!');
