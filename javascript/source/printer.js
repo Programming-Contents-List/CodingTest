cceb1ecf96bc2cbd74795ff9b523e0d68675985c
4Bee <4bee.code@gmail.com>
2024-08-26 15:27:57 +0900

    🚩: srot의 정렬 조건을 통해서 오름 차순을 기준으로 정렬 설정 첫번째 두번째 인자를 비교해서 정렬 시도 오답

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,20 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
+let temp = input.map(el => el.split(' ').map(Number));
+let N = temp[0];
+// console.log(N);
 
-console.log('Hello, World!');
+temp.shift();
+let arr = temp;
+// console.log(arr);
+
+arr.sort((a, b) => {
+  if (a[0] === b[1]) {
+    return a[0] - b[1];
+  } else {
+    return a[1] - b[0];
+  }
+});
+console.log(arr);
