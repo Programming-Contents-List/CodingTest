657c26a9c99f1051594626127941242ab88b68bd
4Bee <4bee.code@gmail.com>
2024-08-27 22:33:42 +0900

    🐛: 예제 입력 5번으로 오답

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -6,24 +6,16 @@ let N = parseInt(input[0]);
 let max = 5;
 let min = 3;
 let result = 0;
-// %, / 이 둘을 다시 짚고 넘어가자.
-// console.log(N % b);
-// console.log(N % a);
-// 왜 while문에서 console.log('e', N % max && min !== 0);와 같이 log를 찍으면 0과 같은 결과가 나오는가?
-//N > (max && min)의 의미는?
-//N % max && N % min !== 0와 N % max !== 0 && N % min !== 0 은 다르다
-//!== 0 은 0이 아닐 경우 true인것이다. 즉, 0이면 false가 되어서 while문에서 벗어난다.
-//조건문을 정할 때 기준이 무엇인지 명확하게 확인과 기준이 필요
 if (N > max && N > min) {
-  while (N % max !== 0 || N % min !== 0) {
+  while (N > 0) {
     if (N % max === 0 && N >= 0) {
       N -= max;
+      console.log(N);
       result++;
     } else if (N % min === 0 && N >= 0) {
       N -= min;
       result++;
     }
-    // console.log('e', N % max && N % min !== 0);
   }
 } else {
   result = -1;
