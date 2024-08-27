4b8bf406afae3328f0b332e7c6f3b17e9ca68a04
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-27 22:11:13 +0900

    🔀: 2839 (#30)
    * 🌱: 알고리즘 개념을 위한 issue templates 생성
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: main 브렌치 환경 초기 세팅을 위한 커밋
    
    * 🚩: 문제에 주어진 조건들을 대입해서 문제를 풀어내지만 소수점을 컨트롤하는 부분이 미흡
    
    * 🚩: 원하는 형태로 순환을 했지만 결과는 의도와 달리 나온다.
    
    * 🚩: 문제의 조건 뿐만 아니라 예상출력의 조건 또한 잘 파악을 해야한다.
    
    * 🚩: 여기서 조건 하나만 변경하면 원하는 정답을 도출 할 수 있다. 수정 필요
    
    * 🚩: 여기서 조건 하나만 변경하면 원하는 정답을 도출 할 수 있다. 수정 필요 및 분석 필요

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,29 +1,32 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
-let length = input[0];
-input.shift();
-
-let arr = input.map(el => el.split(' ')); //.map(Number)
-arr.sort((a, b) => {
-  if (a[0] === b[0]) {
-    return a[1] - b[1];  // y좌표가 같을 때 x좌표 오름차순
-  } else {
-    return a[0] - b[0];  // y좌표 오름차순
+//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
+let N = parseInt(input[0]);
+let max = 5;
+let min = 3;
+let result = 0;
+// %, / 이 둘을 다시 짚고 넘어가자.
+// console.log(N % b);
+// console.log(N % a);
+// 왜 while문에서 console.log('e', N % max && min !== 0);와 같이 log를 찍으면 0과 같은 결과가 나오는가?
+//N > (max && min)의 의미는?
+//N % max && N % min !== 0와 N % max !== 0 && N % min !== 0 은 다르다
+//!== 0 은 0이 아닐 경우 true인것이다. 즉, 0이면 false가 되어서 while문에서 벗어난다.
+//조건문을 정할 때 기준이 무엇인지 명확하게 확인과 기준이 필요
+if (N > max && N > min) {
+  while (N % max !== 0 || N % min !== 0) {
+    if (N % max === 0 && N >= 0) {
+      N -= max;
+      result++;
+    } else if (N % min === 0 && N >= 0) {
+      N -= min;
+      result++;
+    }
+    // console.log('e', N % max && N % min !== 0);
   }
-});
-
-// console.log(arr);
-
-arr.forEach(el => console.log(el.join(' ')));
-
-// console.log(input[0][1])
-// for (let i = 0; i < length; i++) {
-//   let max = Math.max(input[i].length);
-//   // console.log(max)
-//   for (let j = 0; j < max; j++) {
-//     // input[i][j].sort((a, b) => a - b);
-//     console.log(input[i][j]);
-//   }
-// }
+} else {
+  result = -1;
+}
+console.log(N);
+console.log(result);
