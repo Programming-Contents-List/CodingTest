f2f4701ac7c6339536db3a44ba9e82d09f83a0a1
4Bee <4bee.code@gmail.com>
2024-08-27 12:40:14 +0900

    🚩: 문제에 주어진 조건들을 대입해서 문제를 풀어내지만 소수점을 컨트롤하는 부분이 미흡

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,13 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
+let value = input[0];
+console.log(value)
+let a = 3;
+let b = 5;
 
-console.log('Hello, World!');
+while (value !== 0) {
+  value /= 5;
+  console.log(value);
+}
