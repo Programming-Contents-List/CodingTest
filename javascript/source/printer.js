c29c6951fc749b73709738c77a9a203096ea9207
4Bee <4bee.code@gmail.com>
2024-08-27 13:45:44 +0900

    🚩: 문제의 조건 뿐만 아니라 예상출력의 조건 또한 잘 파악을 해야한다.

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -2,23 +2,23 @@ let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
-let value = input[0];
-console.log(value)
+let value = parseInt(input[0]);
 let a = 3;
 let b = 5;
 let result = 0;
 // %, / 이 둘을 다시 짚고 넘어가자.
 // console.log(value % b);
-
-while (value !== 0) {
-  value %= b;
-  result++;
-  if (value % b !== 0) {
-    // console.log('value', value);
-    // break;
-    value %= a;
+console.log(value % a);
+while (value !== 0 && value % a !== 0) {
+  if (value >= b) {
+    value -= b;
     result++;
+  } else if (value % a === 0) {
+    value -= a;
+    result++;
+  } else {
+    result = -1;
   }
 }
-
+console.log(value);
 console.log(result);
