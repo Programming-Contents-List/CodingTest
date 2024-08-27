36665efa34328564730e2dadb0b27494557e0f81
4Bee <4bee.code@gmail.com>
2024-08-27 18:18:33 +0900

    🚩: 여기서 조건 하나만 변경하면 원하는 정답을 도출 할 수 있다. 수정 필요

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -2,23 +2,31 @@ let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
-let value = parseInt(input[0]);
-let a = 3;
-let b = 5;
+let N = parseInt(input[0]);
+let max = 5;
+let min = 3;
 let result = 0;
 // %, / 이 둘을 다시 짚고 넘어가자.
-// console.log(value % b);
-console.log(value % a);
-while (value !== 0 && value % a !== 0) {
-  if (value >= b) {
-    value -= b;
-    result++;
-  } else if (value % a === 0) {
-    value -= a;
-    result++;
-  } else {
-    result = -1;
+// console.log(N % b);
+// console.log(N % a);
+// 왜 while문에서 console.log('e', N % max && min !== 0);와 같이 log를 찍으면 0과 같은 결과가 나오는가?
+//N > (max && min)의 의미는?
+//N % max && N % min !== 0와 N % max !== 0 && N % min !== 0 은 다르다
+//!== 0 은 0이 아닐 경우 true인것이다. 즉, 0이면 false가 되어서 while문에서 벗어난다.
+
+if (N > max && N > min) {
+  while (N % max !== 0 || N % min !== 0) {
+    if (N % max === 0 && N >= 0) {
+      N -= max;
+      result++;
+    } else if (N % min === 0 && N >= 0) {
+      N -= min;
+      result++;
+    }
+    // console.log('e', N % max && N % min !== 0);
   }
+} else {
+  result = -1;
 }
-console.log(value);
+console.log(N);
 console.log(result);
