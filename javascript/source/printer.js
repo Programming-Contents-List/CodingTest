c7325912f56fee31856cf704ebf287f3ef2c7201
4Bee <4bee.code@gmail.com>
2024-08-29 14:00:58 +0900

    🚩: 예제 입력5를 제외한 나머지 예제 입력 통과

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -5,28 +5,37 @@ let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n'
 
 let N = +input[0];  //18, 4, 6, 9, 11
 // 형변환을 하지 않은 이유는 사칙연산을 하면서 암묵적으로 변환이 된다.
-console.log(N);
 let min = 3;
 let max = 5;
 //최소한 적은 개수의 봉지를 이동 해야한다.
 //즉, max값 최대한 활용할 수 있게 해야한다.
 let result = 0;
 
-
+console.log(11 / max);
 //전체적인 루핑은 while문으로 작성 할 것이다. 이유는 조금 더 직관적이다. -> 구체적인 이유가 필요.
 while (N > 0) {  //input값의 공통점은 0보다 큰 양수이다.
-  if (N !== 0) {
+  if (N !== 0 && Number.isInteger(N / max)) { //Number.isInteger(N / max)이는 정수 실수를 판별한다.
     result += 1;
     N -= max;
-    console.log(N);
+    console.log('Max: ', N);
     //해당 조건식이 동작을하고 이후 처리가 될 수 있게 코드를 작성
     if (N < max) {
       result += 1;
       N -= min;
       // return N; //return을 하게 되면 여기서 모든게 끝난다. 즉, 마지막 console.log를 검사하지 않고 끝난다.
+      console.log('Min: ', N);
     }
+  } else if (N !== 0 && Number.isInteger(N / min)) {
+    result += 1;
+    N -= min;
+    // return N; //return을 하게 되면 여기서 모든게 끝난다. 즉, 마지막 console.log를 검사하지 않고 끝난다.
+    console.log('Min: ', N);
   } else {
     console.error("SyntaxError");
+    result = -1;
+    break;  //return으로 처리 하려면 N = -1로 처리 단, 내부 log를 찍어야함
+    // console.log(result);
+    // return -1;
   }
 }
 
