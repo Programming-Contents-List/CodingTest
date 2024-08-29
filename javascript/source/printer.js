d177594a250e2527d19769be8577aed2bc7e6d38
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-29 15:55:54 +0900

    🔀: 2839 retry(1) (#32)
    * 🌱: 알고리즘 개념을 위한 issue templates 생성
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: main 브렌치 환경 초기 세팅을 위한 커밋
    
    * 🚩: 문제에 주어진 조건들을 대입해서 문제를 풀어내지만 소수점을 컨트롤하는 부분이 미흡
    
    * 🚩: 원하는 형태로 순환을 했지만 결과는 의도와 달리 나온다.
    
    * 🚩: 문제의 조건 뿐만 아니라 예상출력의 조건 또한 잘 파악을 해야한다.
    
    * 🚩: 여기서 조건 하나만 변경하면 원하는 정답을 도출 할 수 있다. 수정 필요
    
    * 🚩: 여기서 조건 하나만 변경하면 원하는 정답을 도출 할 수 있다. 수정 필요 및 분석 필요
    
    * 🐛: 예제 입력 5번으로 오답
    
    * 🐛: 마지막 조건문을 변경함으로 예제 입력 5번 통과, 오답
    
    * 🐛: 코드 수정으로 예제 입력 4번에서 오답 후 해결 단, 예제 입력 5번을 명확하게 이유 설명 필요. 정답
    
    * 🚩: 문제의 기본 조건식의 틀 생성
    
    * 🚩: 예시입력1번에 맞는 코드를 작성, 예시입력1번 통과
    
    * 🚩: 예제 입력5를 제외한 나머지 예제 입력 통과
    
    * 🚩: 예제입력 5번과 2번 불통과 문제 해결
    
    * 🚩: 오답
    
    * 🚩: 불필요하게 result갑이 증가된다. 이에 코드의 가독성이 떨어진다.

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -2,31 +2,29 @@ let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
-let N = parseInt(input[0]);
-let max = 5;
+
+let N = +input[0];  //18, 4, 6, 9, 11
 let min = 3;
+let max = 5;
 let result = 0;
-// %, / 이 둘을 다시 짚고 넘어가자.
-// console.log(N % b);
-// console.log(N % a);
-// 왜 while문에서 console.log('e', N % max && min !== 0);와 같이 log를 찍으면 0과 같은 결과가 나오는가?
-//N > (max && min)의 의미는?
-//N % max && N % min !== 0와 N % max !== 0 && N % min !== 0 은 다르다
-//!== 0 은 0이 아닐 경우 true인것이다. 즉, 0이면 false가 되어서 while문에서 벗어난다.
-//조건문을 정할 때 기준이 무엇인지 명확하게 확인과 기준이 필요
-if (N > max && N > min) {
-  while (N % max !== 0 || N % min !== 0) {
-    if (N % max === 0 && N >= 0) {
-      N -= max;
-      result++;
-    } else if (N % min === 0 && N >= 0) {
+
+while (N > 0) {
+  if ((N !== 0 && Number.isInteger(N / max)) || (N !== 0 && (N - max) > max)) { //Number.isInteger(N / max)이는 정수 실수를 판별한다.
+    result += 1;
+    N -= max;
+    if (!Number.isInteger(N / max) && N < max) {
+      result += 1;
       N -= min;
-      result++;
     }
-    // console.log('e', N % max && N % min !== 0);
+  } else if (N !== 0 && N >= min) { //  N % min === 0
+    result += 1;
+    N -= min;
+  } else {
+    result = -1;
+    break;
   }
 } else {
   result = -1;
 }
-console.log(N);
+
 console.log(result);
