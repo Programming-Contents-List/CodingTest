4f9e03a1b7f81bc2971b31d0a83cf85af586b9cf
4Bee <4bee.code@gmail.com>
2024-09-02 22:19:21 +0900

    🚩: 예제입력1에서 기대한 값 출력, 오답

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,27 +1,21 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');  //'\r\n'
 //let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
 
-let N = +input[0];  //18, 4, 6, 9, 11
-let min = 3;
-let max = 5;
-let result = 0;
+// let N = input[0]//.map(el => el.split(' '));
+let N = input.shift();
+let arr = input.map(el => el.split(' '));
+let arrA = arr.slice(0, 3);
+let arrB = arr.slice(3);
+// console.log(arr);
+// arr.map(el => console.log(el));
+let result = [[], [], []];
+arrA.map((el, i) => {
+  el.map((el, j) => {
+    result[i][j] = parseInt(el) + parseInt(arrB[i][j]);
+    // console.log(parseInt(el) + parseInt(arrB[i][j]))
+  });
+});
 
-while (N > 0) {
-  if ((N !== 0 && Number.isInteger(N / max)) || (N !== 0 && (N - max) > max)) { //Number.isInteger(N / max)이는 정수 실수를 판별한다.
-    result += 1;
-    N -= max;
-    if (!Number.isInteger(N / max) && N < max) {
-      result += 1;
-      N -= min;
-    }
-  } else if (N !== 0 && N >= min) { //  N % min === 0
-    result += 1;
-    N -= min;
-  } else {
-    result = -1;
-    break;
-  }
-}
-console.log(result);
+result.forEach(el => console.log(el.join(' ')));
