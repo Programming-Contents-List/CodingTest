cfe8fc0b300c8433891fde66f771246fd349e732
4Bee <109135643+4BFC@users.noreply.github.com>
2024-09-04 22:25:51 +0900

    🔀: Algorithm/dynamic programming (#40)
    * 🌱: 알고리즘 개념을 위한 issue templates 생성
    
    해당 코드는 Github에서 직접 수정
    
    * 🌱: main 브렌치 환경 초기 세팅을 위한 커밋
    
    * 🌱:  README을 수정
    
    GitHub에서 직접 수정한 issue 입니다.
    
    * 🌱: printer.js 파일 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  PR템플릿을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  PR-TEMPLATE를 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정된 커밋입니다.
    
    * 🌱: issue templates 수정
    
    Github에서 작성된 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정된 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🚩: DynamicProgramming을 학습하고 정리
    
    * 🚩: 정리한 글의 파일명 변경
    
    * 🚩: 공부 정리 글을 위한 템플릿 생성

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,7 +1,7 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
-//let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');  //백준 제출
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
 
 let N = +input[0];  //18, 4, 6, 9, 11
 let min = 3;
