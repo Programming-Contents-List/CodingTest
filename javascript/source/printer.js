6a023c270682a209b45764d28f84e7dee9a30b5b
4Bee <4bee.code@gmail.com>
2024-09-06 17:33:48 +0900

    🚩: 문자열의 길이로 sort를 활용해서 정렬하는 방법은 알겠으나, 중복값 제거와 1차적인 정렬 이후 정렬된 값 기준으로 재정렬하는 방법에 대해 문제를 풀어내지 못했음.

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -2,5 +2,15 @@ let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
+let N = input.shift();
+let arr = input;
 
-console.log('Hello, World!');
+//BFAL
+arr.sort((a, b) => {
+  // return a.length - b.length;
+  if (a.length !== b.length) {
+    return a.length - b.length;
+  }
+});
+
+arr.forEach(el => console.log(el))
