755ccbd894bcd443ac419ddae0fcca1aaca9f08f
4Bee <4bee.code@gmail.com>
2024-09-11 23:10:33 +0900

    🚩: if문을 수정 해봤지만 큰수를 비교하는 로직이 필요 즉, for문을 사용해야만 한다.

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -6,26 +6,25 @@ let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
 let N = input.shift();
 let arr = input[0].split(' ').map(el => +el);
 // arr.reverse();
-console.log(arr);
+// console.log(arr);
 // let compare = arr.reverse();
 // console.log(compare);
-let NGE;
+// let NGE;
 let temp = [];
-let i = 0;
-while (i < arr.length) {
-  // NGE = arr.pop();  //뒤에서 부터 pop이되는 걸 확인할 수 있음 따라, reverse 해야함
-  // let current = arr.shift();  // 첫 번째 값을 꺼내고 배열에서 제거
-  let found = false;
-  for (let j = 0; j < arr.length; j++) {
-    //비교한 값은 다시 비교가 되지 않게 shift가 되면서 사라져야함.
-    if (arr[i] < arr[j]) {
-      NGE = arr[j];
-      temp.push(NGE);
-      found = true;
-      break;
-    }
+
+while (arr.length > 0) {
+  //검사한 값은 항시 삭제
+  if (arr.length === 1) {
+    temp.push(-1);
+    break;
   }
-  if (!found) temp.push(-1);
-  i++;
+  let current = arr.shift(); //삭제된 현재 값( = NGE)
+
+  if (current < arr[0]) {
+    temp.push(arr[0]);
+    console.log(arr[0]);
+  } else {
+    temp.push(-1);
+  }
 }
 console.log(temp);
