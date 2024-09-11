2b8a03c1d5fde08db5ecb717d4e76737f2fdc28a
4Bee <4bee.code@gmail.com>
2024-09-11 18:45:57 +0900

    🚩: reverse를 사용하지 않고 shfit로 앞에서 부터 제거하면서 비교하는 코드 구현

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,27 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');  //'\r\n'
 //let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
 
-console.log('Hello, World!');
+let N = input.shift();
+let arr = input[0].split(' ').map(el => +el);
+// arr.reverse();
+console.log(arr);
+// let compare = arr.reverse();
+// console.log(compare);
+let NGE;
+let temp = [];
+let i = 0;
+while (arr.length > 0) {  //arr.length > 0 이렇게 되면 안되는 이유
+  // NGE = arr.pop();  //뒤에서 부터 pop이되는 걸 확인할 수 있음 따라, reverse 해야함
+  if (arr[i] < arr[i + 1]) {
+    console.log(arr[0]);
+    NGE = arr[i + 1];
+    temp.push(NGE);
+  } else {
+    temp.push(-1);
+  }
+  i++;
+  arr.shift();
+}
+console.log(temp);
