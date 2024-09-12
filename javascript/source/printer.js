ea7a3b88edc4b6aecd432df0b78bc83a76118a7c
4Bee <4bee.code@gmail.com>
2024-09-12 12:40:27 +0900

    🚩: 해당 코드의 각 세부적인 설명 추가

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -11,13 +11,15 @@ let arr = input[0].split(' ').map(el => +el);
 // console.log(compare);
 // let NGE;
 let temp = [];
-let stack = [];
-
+let stack = []; //index 정보 따라서 arr[i]보다 늦게 동작해야함.
 for (let i = 0; i < N; i++) {
+  //&& arr[stack[stack.length - 1] -> 초기값 stack 변수를 index 배열로 초기화
+  // = let topIndex = stack[stack.length - 1];  // 스택의 마지막 인덱스 추출
+  //결론: stack[stack.length - 1]는 Number이다. 즉, 초기값은 index 3이며 3을 참조하는 값은 7이다.
   while (stack.length && arr[stack[stack.length - 1]] < arr[i]) {
-    temp[stack.pop()] = arr[i];
+    temp[stack.pop()] = arr[i]; //stack에서 pop을 해도 index 값이 pop되기 때문에 반환값 역시 number이다.
   }
-  stack.push(i);
+  stack.push(i);  //stack에 i가 들어감 이유는 배열의 크기를 맞추기 위해
 }
 
 while (stack.length) {
