c9f563ae4919a4f92ad2be64124e8eeb78ff4804
4Bee <4bee.code@gmail.com>
2024-09-12 12:49:22 +0900

    🚩: 코드의 전체적 흐름의 이해를 위해 변수명 변경

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -10,20 +10,20 @@ let arr = input[0].split(' ').map(el => +el);
 // let compare = arr.reverse();
 // console.log(compare);
 // let NGE;
-let temp = [];
-let stack = []; //index 정보 따라서 arr[i]보다 늦게 동작해야함.
+let result = [];
+let temp = []; //index 정보 따라서 arr[i]보다 늦게 동작해야함.(= index, stack)
 for (let i = 0; i < N; i++) {
-  //&& arr[stack[stack.length - 1] -> 초기값 stack 변수를 index 배열로 초기화
-  // = let topIndex = stack[stack.length - 1];  // 스택의 마지막 인덱스 추출
-  //결론: stack[stack.length - 1]는 Number이다. 즉, 초기값은 index 3이며 3을 참조하는 값은 7이다.
-  while (stack.length && arr[stack[stack.length - 1]] < arr[i]) {
-    temp[stack.pop()] = arr[i]; //stack에서 pop을 해도 index 값이 pop되기 때문에 반환값 역시 number이다.
+  //&& arr[temp[temp.length - 1] -> 초기값 temp 변수를 index 배열로 초기화
+  // = let topIndex = temp[temp.length - 1];  // 스택의 마지막 인덱스 추출
+  //결론: temp[temp.length - 1]는 Number이다. 즉, 초기값은 index 3이며 3을 참조하는 값은 7이다.
+  while (temp.length && arr[temp[temp.length - 1]] < arr[i]) {
+    result[temp.pop()] = arr[i]; //stack에서 pop을 해도 index 값이 pop되기 때문에 반환값 역시 number이다.
   }
-  stack.push(i);  //stack에 i가 들어감 이유는 배열의 크기를 맞추기 위해
+  temp.push(i);  //stack에 i가 들어감 이유는 배열의 크기를 맞추기 위해
 }
 //결론적으로 index값을 가지고 있는 배열과 값을 가지고 있는 본래의 배열을 가지고 동작한다.
-while (stack.length) {
-  temp[stack.pop()] = -1;
+while (temp.length) {
+  result[temp.pop()] = -1;
 }
 
-console.log(temp.join(' '));
+console.log(result.join(' '));
