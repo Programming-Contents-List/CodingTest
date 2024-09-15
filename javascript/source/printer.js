241aed723be34a9f8cedad51a033a11f35615131
4Bee <4bee.code@gmail.com>
2024-09-15 21:43:36 +0900

    🚩: while문 영역을 for문과 if문으로 구성

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -16,10 +16,14 @@ for (let i = 0; i < N; i++) {
   //&& arr[temp[temp.length - 1] -> 초기값 temp 변수를 index 배열로 초기화
   // = let topIndex = temp[temp.length - 1];  // 스택의 마지막 인덱스 추출
   //결론: temp[temp.length - 1]는 Number이다. 즉, 초기값은 index 3이며 3을 참조하는 값은 7이다.
-  while (temp.length && arr[temp[temp.length - 1]] < arr[i]) {
-    console.log(temp[temp.length - 1]);
-    console.log(temp);
-    result[temp.pop()] = arr[i]; //stack에서 pop을 해도 index 값이 pop되기 때문에 반환값 역시 number이다.
+  for (let j = temp.length - 1; j >= 0; j--) {
+    if (arr[temp[j]] < arr[i]) {
+      console.log(temp[j]);
+      console.log(temp);
+      result[temp.pop()] = arr[i]; // stack에서 pop을 해도 index 값이 pop되기 때문에 반환값 역시 number이다.
+    } else {
+      break; // 조건을 만족하지 않으면 더 이상 탐색할 필요가 없으므로 중단
+    }
   } //while문 중괄호
   //for문 영역
   temp.push(i);  //stack에 i가 들어감 이유는 배열의 크기를 맞추기 위해
