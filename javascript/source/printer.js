e5761b75ab7827c1df96e9a24d33abdf37c2cf78
4Bee <4bee.code@gmail.com>
2024-09-24 15:43:38 +0900

    🚩: stack으로 문제를 풀어보려고 시도 단, 어떤 규칙으로, 어떤 기준으로 push하고 pop해야 할지 잘 모르겠음

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,4 +1,25 @@
+const { stat } = require('fs');
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');  //\r\n
-//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
+let [N, ...input] = require('fs').readFileSync(inputPath).toString().trim().split('\n');  //\r\n
+//let [N, input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
+
+console.log(input);
+let count = 0;
+let state = false;
+let stack = [];
+//연속적이면 ok
+//ab 다음 a가 나오면 not ok 따라서, abb는 not group
+input.map(el => {
+  el.map((el, index) => {
+    if (state != true) {
+      if (stack.length === 0) {
+        stack.push(+el[index]);
+        console.log(stack);
+      } else if (stack[index] === +el[index + 1]) {
+        let what = stack.pop();
+        console.log(what);
+      }
+    }
+  })
+})
