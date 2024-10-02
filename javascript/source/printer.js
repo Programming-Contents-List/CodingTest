e0658c995e1ba71eb570e605517b1d9c980d53ed
4Bee <4bee.code@gmail.com>
2024-10-03 00:08:18 +0900

    🚩: for문을 사용해서 문제 해결

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,19 +1,18 @@
-const { stat } = require('fs');
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');  //\r\n
 //let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
 
 let N = parseInt(input[0]);
-console.log(N);
-let temp = 0;
-let result;
+let first = 0;
+let second = 1;
+let result = 0;
 
-for (let i = 0; i < N; i++) {
-  let current = i;
-  temp = current + 1;
-  current += temp;
-  // result = current + temp;
-  console.log(`current : ${current}/ temp : ${temp}`);
-  // console.log(result);
+for (let i = 2; i <= N; i++) {
+  result = first + second;
+  first = second;
+  second = result;
+  console.log(`항 ${i}: ${result}`);
 }
+
+console.log(`피보나치 수열의 ${N}번째 항은 ${result}입니다.`);
