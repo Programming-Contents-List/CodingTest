8bb3b80e9993f857e3115395222783b56243257d
4Bee <4bee.code@gmail.com>
2024-10-03 00:13:49 +0900

    🚩: 예외적인 상황을 위한 조건문 추가

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -4,8 +4,8 @@ let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
 
 let N = parseInt(input[0]);
-let first = 0;
-let second = 1;
+let first = 0;  //current
+let second = 1;  //temp
 let result = 0;
 
 for (let i = 2; i <= N; i++) {
@@ -13,6 +13,12 @@ for (let i = 2; i <= N; i++) {
   first = second;
   second = result;
   console.log(`항 ${i}: ${result}`);
+}
+
+//  N이 1일 경우의 피보나치
+if (N === 1) {
+  result = 1;
+  console.log(`항 1: ${result}`);
 }
 
 console.log(`피보나치 수열의 ${N}번째 항은 ${result}입니다.`);
