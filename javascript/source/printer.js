74d72dbacca8e6e731360303f0c5d25238df6c2d
4Bee <4bee.code@gmail.com>
2024-10-03 01:46:53 +0900

    🚩: 문자열을 map으로 접근 시도, error

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,10 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');  //\r\n
 //let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
 
-console.log('Hello, World!');
+console.log(input[0].length);
+input[0].map(el => {
+  console.log(el);
+})
+
