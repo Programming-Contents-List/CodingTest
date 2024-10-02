a150245a29941008f1d975d92a1ae9e7d616905b
4Bee <4bee.code@gmail.com>
2024-10-03 00:04:32 +0900

    🚩: 일반적으로 문제에서 주어진 식이 아닌 for문이라는 한정에서 문제를 풀려고 시도

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,25 +1,19 @@
 const { stat } = require('fs');
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let [N, ...input] = require('fs').readFileSync(inputPath).toString().trim().split('\n');  //\r\n
-//let [N, input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');  //\r\n
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
 
-console.log(input);
-let count = 0;
-let state = false;
-let stack = [];
-//연속적이면 ok
-//ab 다음 a가 나오면 not ok 따라서, abb는 not group
-input.map(el => {
-  el.map((el, index) => {
-    if (state != true) {
-      if (stack.length === 0) {
-        stack.push(+el[index]);
-        console.log(stack);
-      } else if (stack[index] === +el[index + 1]) {
-        let what = stack.pop();
-        console.log(what);
-      }
-    }
-  })
-})
+let N = parseInt(input[0]);
+console.log(N);
+let temp = 0;
+let result;
+
+for (let i = 0; i < N; i++) {
+  let current = i;
+  temp = current + 1;
+  current += temp;
+  // result = current + temp;
+  console.log(`current : ${current}/ temp : ${temp}`);
+  // console.log(result);
+}
