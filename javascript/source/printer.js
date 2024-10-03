68c791102cb9652d538490d19991ef544a85100c
4Bee <4bee.code@gmail.com>
2024-10-03 16:34:51 +0900

    🚩: 시간초과로 인한 에러, 변수가 너무 불필요하게 많음 수정 필요

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,3 +1,4 @@
+const { truncate } = require('fs');
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');  //\r\n
@@ -6,42 +7,28 @@ let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
 // console.log(input[0].length);
 
 let N = input[0];
-let original = N;
+let originN = N;
 let count = 0;
-// let count = 0;
-// let temp = 0;
-// let current = 0;
-// let charSum = '';
-//왜 Array.from인가?
-// Array.from(N).map(el => {
-//   // console.log(el, index);
-//   // temp = +el[1]; //map의 el은 el로만 접근, index로 접근하려면 foreach나 for문으로 접근
-//   temp = +el; //하지만 마지막 숫자만 담기면 된다.
-//   current += +el;
-//   charSum = temp.toString() + current.toString();
-//   if (charSum != input[0]) {
-//     return count++;
-//   }
-// })
-// console.log(temp.toString());
-// console.log(current.toString());
-// console.log(charSum);
-//Array.from(N)은 새로운 배열을 만들어내기에 함수로 구현함에 적합하지 않음
+let NewN = '';
+let temp = 0;
 
-function Transfer(N, count = 0) {
-  // 각 자리수를 더하여 새로운 수를 생성
-  let sum = (+N[0]) + (+N[1]);
-  let newNumber = N[1] + (sum % 10);  // 10으로 나눈 나머지 값을 즉, 뒷자리의 숫자만 남기기 위함이다.
-  //예를 들어 14이면 10을 나눠서 나뉜 나머지 값 4만 남는다.
+while (true) {
+  temp = (+N[0]) + (+N[1]);
+  NewN = N[1] + (temp % 10).toString();
+  count++;
 
-  count++;  // 반복 횟수 증가
-
-  if (newNumber === original) {
-    return count;  // 원래의 수로 돌아왔으면 반복 횟수 반환
+  if (NewN === originN) {
+    break;
   } else {
-    return Transfer(newNumber, count);  // 재귀 호출로 다음 수 계산
+    N = NewN;
   }
 }
-// 결과 출력
-console.log(Transfer(N, count));
+
+console.log(N[0]);
+console.log(N[1]);
+console.log(NewN);
+console.log(N);
+console.log(count);
+//Array.from(N)은 새로운 배열을 만들어내기에 함수로 구현함에 적합하지 않음
+
 
