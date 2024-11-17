bcebd9c2ab92dc4e2eda4586f64e87fe44c751de
4Bee <4bee.code@gmail.com>
2024-11-17 23:12:41 +0900

    ✨: 시간초과, 제출 시 path 부분 stdin.txt에서 txt제거 필요

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,53 @@
 let path = require('path');
-let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
+let inputPath = path.join(__dirname, '/dev/stdin.txt'); // __dirname은 현재 스크립트의 디렉토리 절대경로
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
+input.shift();
 
-console.log('Hello, World!');
+let deck = [];
+for (let el of input) {
+  // console.log(el);
+  if (el.includes('push_back')) {
+    let value = el.split(' ')[1];
+    // console.log(value);
+    deck.push(value);
+  } else if (el.includes('push_front')) {
+    let value = el.split(' ')[1];
+    // console.log(value);
+    deck.unshift(value);
+  } else if (el.includes('pop_back')) {
+    // console.log(value);
+    if (deck.length > 0) {
+      console.log(deck.pop());
+    } else {
+      console.log(-1);
+    }
+  } else if (el.includes('pop_front')) {
+    if (deck.length > 0) {
+      console.log(deck.shift());
+    } else {
+      console.log(-1);
+    }
+  } else if (el.includes('size')) {
+    console.log(deck.length);
+  } else if (el.includes('empty')) {
+    if (deck.length === 0) {
+      console.log(1);
+    } else {
+      console.log(0);
+    }
+  } else if (el.includes('front')) {
+    if (deck.length > 0) {
+      console.log(deck[0]);
+    } else {
+      console.log(-1);
+    }
+  } else if (el.includes('back')) {
+    if (deck.length > 0) {
+      console.log(deck[deck.length - 1]);
+    } else {
+      console.log(-1);
+    }
+  }
+}
+// console.log(deck);
