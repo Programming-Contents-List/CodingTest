2637416ae240e66cc42512b75c7c39530e7a2cee
4Bee <4bee.code@gmail.com>
2024-11-24 23:39:32 +0900

    ✨: infinity 문법을 사용해서 최대값 구하기

---
+++ javascript/manager/programmers/Extra/maxValue.js
@@ -0,0 +1,10 @@
+let array = [10, 20, 30, 13, 25];
+let maxValue = -Infinity; // 초기값을 가장 작은 값으로 설정
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > maxValue) {
+    maxValue = array[i]; // 더 큰 값을 만나면 maxValue 갱신
+  }
+}
+
+console.log(maxValue); // 출력: 30
