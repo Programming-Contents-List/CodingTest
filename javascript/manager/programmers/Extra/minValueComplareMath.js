c8776bbae229d66fa052b38436abf1e996253696
4Bee <4bee.code@gmail.com>
2024-12-15 22:17:19 +0900

    ✨: Math.min을 배열로 활용한 최소값 구하기

--- javascript/manager/programmers/Extra/minValueComplareMath.js
+++ javascript/manager/programmers/Extra/minValueComplareMath.js
@@ -5,3 +5,12 @@ let result = Math.min(std, target); // a(10)과 b(5)를 비교
 console.log(result); // 출력: 5
 
 //여기서 a는 비교 기준이고 b는 비교 대상이다.
+
+// 배열 값일 경우 다음과 같다.
+let array = [10, 20, 30, 13, 25];
+let standard = 15; // 비교 기준 값
+
+for (let target of array) {
+  let result = Math.min(standard, target); // standard의 현재 배열 요소(target)를 비교
+  console.log(`standard: ${standard}, target: ${target}, result: ${result}`);
+}
