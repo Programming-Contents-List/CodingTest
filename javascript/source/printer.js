ad176c34ecab1e58066a7c7ea07b8915870654bf
4Bee <4bee.code@gmail.com>
2024-12-15 22:54:17 +0900

    ✨: 불필요한 코드 제거

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,4 +1,3 @@
-const { stat } = require("fs");
 let path = require("path");
 let inputPath = path.join(__dirname, "/dev/stdin"); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require("fs").readFileSync(inputPath).toString().trim().split("\n"); //\r\n
