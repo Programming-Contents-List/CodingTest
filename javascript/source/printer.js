5eaf11e01f414e44fdc85337b307933e3e39b848
4Bee <109135643+4BFC@users.noreply.github.com>
2024-12-17 18:39:10 +0900

    🔀: 16173 코드 분석(Array.from과 this) (#71)
    * ✨: 16173 코드 분석
    
    * ✨: array.from의 모든 인자를 활용한 예시
    
    * ✨: array.from의 첫번째 인자 arraySize를 활용한 예시
    
    * ✨: array.from의 두 번째 인자 mapFn:el을 활용한 예시
    
    * ✨: array.from의 두 번째 인자 mapFn:el을 활용한 예시
    
    * ✨: array.from의 두 번째 인자 thisArg를 활용한 예시
    
    * ✨: This의 개념과 this와 함수의 연관관계
    
    * ✨: DFS로 문제 해결 및 문제 조건에 맞게 코드 구현

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,40 +1,68 @@
-let path = require('path');
-let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');  //'\r\n'
+let path = require("path");
+let inputPath = path.join(__dirname, "/dev/stdin"); // __dirname은 현재 스크립트의 디렉토리 절대경로
+let input = require("fs")
+  .readFileSync(inputPath)
+  .toString()
+  .trim()
+  .split("\r\n");
 //let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
-const board = input.map((el) => el.split(" ").map(Number)); // 숫자판 생성
-const uniqueNumbers = new Set(); // 중복 제거를 위한 Set
-const directions = [
-  [-1, 0], // 위
-  [1, 0],  // 아래
-  [0, -1], // 왼쪽
-  [0, 1],  // 오른쪽
+
+console.log(input);
+let limit = Number(input.shift());
+let array = input.map((el) => el.split(" ").map(Number));
+
+// 방문 여부 가능 크기 - array 크기와 동일하게 생성
+let n = array.length; // 행 개수
+let m = array[0].length; // 열 개수
+// 방문 여부 기록
+let stack = [[0, 0]]; // 시작 지점 (0, 0)
+let visited = Array.from({ length: n }, () => Array(n).fill(false));
+visited[0][0] = true;
+
+console.log(visited);
+
+//도착 여부 플래그
+let flag = false;
+
+// 젤리 방향 설정
+let dir = [
+  [0, 1],
+  [1, 0],
 ];
 
-let N = input.shift();
-let arr = input[0].split(' ').map(el => +el);
-// arr.reverse();
-// console.log(arr);
-// let compare = arr.reverse();
-// console.log(compare);
-// let NGE;
-let result = [];
-let temp = []; //index 정보 따라서 arr[i]보다 늦게 동작해야함.(= index, stack)
-for (let i = 0; i < N; i++) {
-  //&& arr[temp[temp.length - 1] -> 초기값 temp 변수를 index 배열로 초기화
-  // = let topIndex = temp[temp.length - 1];  // 스택의 마지막 인덱스 추출
-  //결론: temp[temp.length - 1]는 Number이다. 즉, 초기값은 index 3이며 3을 참조하는 값은 7이다.
-  while (temp.length && arr[temp[temp.length - 1]] < arr[i]) {
-    console.log(temp[temp.length - 1]);
-    console.log(temp);
-    result[temp.pop()] = arr[i]; //stack에서 pop을 해도 index 값이 pop되기 때문에 반환값 역시 number이다.
-  } //while문 중괄호
-  //for문 영역
-  temp.push(i);  //stack에 i가 들어감 이유는 배열의 크기를 맞추기 위해
-}
-//결론적으로 index값을 가지고 있는 배열과 값을 가지고 있는 본래의 배열을 가지고 동작한다.
-while (temp.length) {
-  result[temp.pop()] = -1;
+// DFS
+while (stack.length > 0) {
+  const [recentX, recentY] = stack.pop();
+  // 젤리가 grid에서 얻게되는 현재 위치
+  let getPosition = array[recentX][recentY];
+
+  // 행과 열의 도착 지점이 -1이면 종료
+  if (recentX === n - 1 && recentY === m - 1) {
+    flag = true;
+    break;
+  }
+
+  // 방향에 따라 이동
+  for (let i = 0; i < dir.length; i++) {
+    const [aliasX, aliasY] = dir[i]; // 구조할당으로 dir의 배열값을 nextX와 nextY로 선언
+    const nextX = recentX + aliasX * getPosition; // 다음 x 좌표 구하기
+    // 다음 좌표 이동 값을 구하기 위해 젤리가 움직일 수 있는 방향 값에 젤리가 현재 있는 grid 자리의 이동 가능 수를 0,1로 설정된 값을 곱한 후 현재 위치의 행 좌표에 더한다.
+    const nextY = recentY + aliasY * getPosition; // 다음 y 좌표 구하기
+    // 다음 좌표 이동 값을 구하기 위해 젤리가 움직일 수 있는 방향 값에 젤리가 현재 있는 grid 자리의 이동 가능 수를 0,1로 설정된 값을 곱한 후 현재 위치의 열 좌표에 더한다.
+
+    // nextX, nextY가 배열 범위 내에 있는지 확인하고 방문 여부를 처리
+    // 이를 검사하는 이유는 젤리가 grid에서 얻은 이동 가능 다음 위치 결과 값이 n과 m의 범위를 넘어갈 수 있기 때문이다.
+    if (
+      nextX >= 0 &&
+      nextX < n &&
+      nextY >= 0 &&
+      nextY < m &&
+      !visited[nextX][nextY] // visited에 이동 했던 방문 위치 여부 확인
+    ) {
+      stack.push([nextX, nextY]); // 새로운 위치를 스택에 추가
+      visited[nextX][nextY] = true; // 방문 처리
+    }
+  }
 }
 
-console.log(result.join(' '));
+console.log(flag ? "HaruHaru" : "Hing");
