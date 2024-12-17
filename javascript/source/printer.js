f9c94cb74caa208a31246146bbfef4432fe8b5f3
4Bee <4bee.code@gmail.com>
2024-12-17 21:09:36 +0900

    ✨: input 값을 가공하는 것에 문제가 있었다. 받은 문자열을 잘 가공 해야한다.

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,51 +1,20 @@
 let path = require("path");
 let inputPath = path.join(__dirname, "/dev/stdin"); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require("fs")
-  .readFileSync(inputPath)
-  .toString()
-  .trim()
-  .split("\r\n");
-//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
+let input = require("fs").readFileSync(inputPath).toString().trim().split("");
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('');  //백준 제출
 
-let array = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "g",
-];
+let array = "abcdefghijklmnopqrstuvwxyz";
 
 const n = array.length;
 const exit = Array(n).fill(0);
-
+// console.log(input);
 input.forEach((el) => {
-  el.split("").forEach((char) => {
-    let index = array.indexOf(char);
-    if (index !== -1) {
-      exit[index] += 1;
-    }
-  });
+  let index = array.indexOf(el);
+  // indexOf는 배열이 존재하지 않으면 -1을 반환한다. 그래서 예외 처리를 하기 위해서 조건문을 추가 했다.
+  if (index !== -1) {
+    exit[index] += 1;
+    // exit[index]++;
+  }
 });
 
 console.log(exit.join(" "));
