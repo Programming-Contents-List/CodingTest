03b0792f037c5a8f59ec5c46730c1291c9566de9
4BFC <4bee.code@gamil.com>
2025-03-09 20:14:40 +0900

    ✅: 각 파일 경로 업데이트

--- javascript/source/1259/printer.js
+++ javascript/source/1259/printer.js
@@ -1,5 +1,5 @@
 let path = require("path");
-let inputPath = path.join(__dirname, "/dev/stdin"); // __dirname은 현재 스크립트의 디렉토리 절대경로
+let inputPath = path.join(__dirname, "../dev/stdin"); // __dirname은 현재 스크립트의 디렉토리 절대경로
 let input = require("fs")
   .readFileSync(inputPath)
   .toString()
