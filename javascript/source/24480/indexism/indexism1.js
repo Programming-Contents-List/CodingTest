2cf707b9dabd77d044ef7594bddaf0365e42d3bd
4BFC <4bee.code@gamil.com>
2025-03-09 19:52:34 +0900

    ✨: indexsim 문제 해결

--- javascript/source/24480/indexism/indexism1.js
+++ javascript/source/24480/indexism/indexism1.js
@@ -4,32 +4,36 @@
  * 당신은 특정한 순서대로 집을 방문할 것이다.
  * 각 집을 방문할 때 방문한 순서를 기록하는 배열을 만들어라.
  * 방문하지 않은 집은 0으로 표시해야 한다.
+ *
+ * 입력 예제
+ * 5
+ * 3 1 4
+ *
+ * 출력 예제
+ * 2 0 1 3 0
+ *
+ * 입력 예제
+ * 7
+ * 5 3 7 1
+ *
+ * 출력 예제
+ * 4 0 2 0 1 0 3
  */
 
 let input = `5
 3 1 4`.split("\n");
 let N = Number(input.shift());
-let graph = input.map((el) => el.split(" ").map(Number));
-let init = graph[0][0];
+//불필요한 중첩 배열
+// let graph = input.map((el) => el.split(" ").map(Number));
+let graph = input[0].split(" ").map(Number);
 console.log(graph);
 
-let visited = new Array(N + 1).fill(false);
 let result = new Array(N + 1).fill(0);
-let stack = [[init]];
-let count = 0;
-console.log(stack);
+let count = 1;
 
-while (stack.length > 0) {
-  const current = stack.pop();
-  //   console.log(stack[].length);
-  for (let nextNode of stack[count]) {
-    console.log(nextNode);
-    // const nextNode = graph[current][i];
-    // console.log("graph", nextNode);
-  }
-  //   if (!visited === current) {
-  //     visited[current] = true;
-  //     result[current].push();
-  //   }
+for (let i = 0; i < graph.length; i++) {
+  const visited = graph[i];
+  result[visited] = count++;
 }
-// console.log(result);
+
+console.log(result.slice(1).join(" "));
