f39e87a9340afa7353d7c3dbbe286961ce404a7a
4Bee <4bee.code@gmail.com>
2025-08-14 19:29:10 +0900

    🔀:  연결리스트 with python(2) (#85)
    * ✨: 파이썬으로 linkdelist 구현
    
    * ✨: 기존 코드를 함수를 사용해서 명확하게 관심사 분리

--- algorithm/python/manager/alogrithm/linkedlist.py
+++ algorithm/python/manager/alogrithm/linkedlist.py
@@ -3,20 +3,28 @@ class Node() :
         self.data = data
         self.next = None
 
+def createNode(node):
+    while node:
+        if node.next is None:
+            node.next = Node(4)
+        break
+    node = node.next
+    return node
+    
+def printNode(node):
+    while node:
+        print(node.data, end=" -> ")
+        node = node.next
+    print("Node")
+
 head = Node(1)
 head.next = Node(2)
 head.next.next = Node(3)
 
 node = head
-while node:
-    if node.next is None:
-        node.next = Node(4)
-        break
-    node = node.next
 
-# 연결 리스트 출력
-node = head
-while node:
-    print(node.data, end=" -> ")
-    node = node.next
-print("None")
+
+createNode(node)
+printNode(node)
+
+
